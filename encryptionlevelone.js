17284da717a29a3afc3d74501eeed8ac46eb8489
Spottedwoot <104224892+Spottedwoot@users.noreply.github.com>
2025-06-02 08:58:13 -0700

    Create encryptionlevelone.js

---
+++ encryptionlevelone.js
@@ -0,0 +1,32 @@
+const small_primes = [2,3,5,7,11,13,17,19];
+let random1 = Math.floor(Math.random() * (8));
+let random2 = Math.floor(Math.random() * (8));
+document.getElementById("question").innerHTML= "("+small_primes[random1].toString() + "-1)*(" + small_primes[random2].toString()+ "-1)";
+var answer = (small_primes[random1]-1)*(small_primes[random2]-1);
+
+let miliseconds = 0;
+const timerDisplay = document.getElementById('test'); // Assuming you have an element with id="timer"
+
+function updateTimer() {
+  miliseconds++;
+  timerDisplay.innerHTML = miliseconds/250;
+}
+
+const intervalId = setInterval(updateTimer, 1);
+var form = document.getElementById("button");
+form.onclick = function(e){
+  e.preventDefault();
+if(document.getElementById("inputbox").value===answer.toString()){
+  if(document.getElementById('test').innerHTML==="Wrong"){
+    document.getElementById('test').innerHTML="Wrong";
+  }
+  else{
+    var outcome = "You got it in: ";
+  clearInterval(intervalId);
+  document.getElementById('test').innerHTML=outcome + timerDisplay.innerHTML + " seconds";}
+  }
+else{
+  clearInterval(intervalId);
+  document.getElementById('test').innerHTML="Wrong";}
+}
+ // Update every second
